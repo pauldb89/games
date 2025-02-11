Fix tests and improve hint computation logic.

games/tests/wordle/test_environment.py
@@ -1,5 +1,6 @@
 
 import more_itertools
+import numpy as np
 from games.wordle.consts import EXACT_MATCH
 from games.wordle.consts import LETTER_MATCH
 from games.wordle.consts import NO_MATCH
@@ -20,8 +21,7 @@ def test_environment():
     letters = ["r", "a", "i", "s", "e", "s", "o", "u", "l", "s", "b", "o", "n", "u", "s"]
     hints = [
         [NO_MATCH, NO_MATCH, NO_MATCH, LETTER_MATCH, NO_MATCH],
-        # TODO(pauldb): Make the compute_hint logic more sophisticated to track number of matches.
-        [LETTER_MATCH, EXACT_MATCH, LETTER_MATCH, NO_MATCH, EXACT_MATCH],
+        [NO_MATCH, EXACT_MATCH, LETTER_MATCH, NO_MATCH, EXACT_MATCH],
         [EXACT_MATCH, EXACT_MATCH, EXACT_MATCH, EXACT_MATCH, EXACT_MATCH],
     ]
 
@@ -31,7 +31,7 @@ def test_environment():
     assert state.guesses == []
     assert state.hints == []
     for idx, letter in enumerate(letters):
-        state = env.step(action=Action(letter=letter, mask=[]))
+        state = env.step(action=Action(letter=letter, mask=[], lprobs=np.random.randn(26)))
 
         guesses = ["".join(guess) for guess in more_itertools.chunked(letters[:idx+1], n=WORD_LENGTH)]
         assert state.guesses == guesses

games/tests/wordle/test_reward.py
@@ -1,3 +1,4 @@
+import numpy as np
 from games.wordle.consts import EXACT_MATCH, LETTER_MATCH, NO_MATCH
 from games.wordle.environment import Environment, Transition
 from games.wordle.reward import Reward
@@ -13,7 +14,7 @@ def test_reward() -> None:
 
     transitions = []
     for letter in letters:
-        action = Action(letter=letter, mask=[])
+        action = Action(letter=letter, mask=[], lprobs=np.random.randn(26))
         next_state = env.step(action)
         transitions.append(Transition(source_state=state, target_state=next_state, action=action))
         state = next_state
@@ -21,5 +22,5 @@ def test_reward() -> None:
     reward = Reward(win_reward=100, no_match_reward=-2, letter_match_reward=-1, exact_match_reward=0)
 
     rewards = reward(transitions)
-    expected_rewards = [-2, -2, -2, -1, -2, -1, 0, -1, -2, 0, 0, 0, 0, 0, 100]
+    expected_rewards = [-2, -2, -2, -1, -2, -2, 0, -1, -2, 0, 0, 0, 0, 0, 100]
     assert rewards == expected_rewards